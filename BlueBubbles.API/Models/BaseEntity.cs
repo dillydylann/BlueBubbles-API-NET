e1e13b74cebc8f7559e79f7f3be35825f32d3c53
Dylan <dylan@dylanbriedis.com>
2022-10-08 22:58:45 -0700

    Add base model classes

---
+++ BlueBubbles.API/Models/BaseEntity.cs
@@ -0,0 +1,32 @@
+﻿// This file is a part of the BlueBubbles .NET API library
+// Copyright (c) 2022 Dylan Briedis <dylan@dylanbriedis.com>
+
+using System;
+using Newtonsoft.Json;
+
+namespace BlueBubbles.API.Models
+{
+    /// <summary>
+    /// Represents the base class for all iMessage entities.
+    /// </summary>
+    public class BaseEntity : IEquatable<BaseEntity>
+    {
+        /// <summary>
+        /// Gets the original row ID in the server database.
+        /// </summary>
+        [JsonProperty("originalROWID")]
+        public int OriginalRowId { get; private set; }
+
+        /// <inheritdoc/>
+        public override bool Equals(object obj) => Equals(obj as BaseEntity);
+
+        /// <inheritdoc/>
+        public bool Equals(BaseEntity other) => !(other is null) && OriginalRowId == other.OriginalRowId;
+
+        /// <summary>
+        /// Returns the hash code for this object.
+        /// </summary>
+        /// <inheritdoc path="//returns"/>
+        public override int GetHashCode() => -1962740069 + OriginalRowId.GetHashCode();
+    }
+}
---
+++ BlueBubbles.API/Models/PrivateAPIAttribute.cs
@@ -0,0 +1,15 @@
+﻿// This file is a part of the BlueBubbles .NET API library
+// Copyright (c) 2022 Dylan Briedis <dylan@dylanbriedis.com>
+
+using System;
+
+namespace BlueBubbles.API.Models
+{
+    /// <summary>
+    /// Marks a method as part of the Private API.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Method)]
+    public sealed class PrivateAPIAttribute : Attribute
+    {
+    }
+}
